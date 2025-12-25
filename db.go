506dcc4dba9ccba1d99e7cee1b37855f59a31195
momin-mostafa <mominmostafa.g@gmail.com>
2025-12-25 20:23:06 +0600

    add test utils

---
+++ db.go
@@ -0,0 +1,30 @@
+package testutil
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupMockDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("failed to create sqlmock: %v", err)
+	}
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("failed to open gorm db: %v", err)
+	}
+
+	return gormDB, mock
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+}
